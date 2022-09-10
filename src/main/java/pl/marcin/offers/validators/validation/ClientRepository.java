42f55de6ad8e6f1037e5429f7cf14f845dc20b5a
Zwidek <82658699+Zwidek@users.noreply.github.com>
2022-09-10 17:53:22 +0200

    Delete src/main/java/pl/marcin/offers/validators directory

--- src/main/java/pl/marcin/offers/validators/validation/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.marcin.offers.validators.validation;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientRepository extends CrudRepository<Client, Long> {
-    boolean existsByUsername(String username);
-}
